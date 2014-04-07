27b279cb7bce90015da93cf488df4bf800edf233
tjdfalcon <tjdfalcon@gmail.com>
2014-04-06 23:16:50 -0700

    Some sms stuff

--- TextSecretary/src/edu/gonzaga/textsecretary/SMS_Service.java
+++ TextSecretary/src/edu/gonzaga/textsecretary/SMS_Service.java
@@ -1,8 +1,13 @@
 package edu.gonzaga.textsecretary;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.telephony.SmsMessage;
+import android.util.Log;
 
 public class SMS_Service extends Service{
 
@@ -12,4 +17,36 @@ public class SMS_Service extends Service{
 		return null;
 	}
 
+	@Override
+	public void onCreate(){
+		
+	}
+	
+	public class SMS_Listener extends BroadcastReceiver {
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			if(intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")){
+				Bundle bundle = intent.getExtras();
+				SmsMessage[] msgs = null;
+				String msg_from;
+				if (bundle != null){
+					try{
+						Object[] pdus = (Object[]) bundle.get("pdus");
+	                    msgs = new SmsMessage[pdus.length];
+						for(int i = 0; i < msgs.length; i++){
+	                        msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
+	                        msg_from = msgs[i].getOriginatingAddress();
+	                        //String msgBody = msgs[i].getMessageBody();
+	                    }
+					} catch(Exception e){
+						Log.d("SMS", e.getMessage());
+					}
+				}
+			}
+		}
+		
+		
+	}
+	
 }
