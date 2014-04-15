e95c16ab0b60dfc173e5dd84d669e06bee1742ef
tjdfalcon <tjdfalcon@gmail.com>
2014-04-14 23:48:12 -0700

    formatting

--- TextSecretary/src/edu/gonzaga/textsecretary/Notification_Service.java
+++ TextSecretary/src/edu/gonzaga/textsecretary/Notification_Service.java
@@ -13,43 +13,43 @@ public class Notification_Service{
 	   private NotificationManager mNotificationManager;
 	   Context mContext;
 	   
-public Notification_Service (Context context){
-	mContext = context;
-}
+	public Notification_Service (Context context){
+		mContext = context;
+	}
 	public void displayNotification(String number) {
-		      Log.d("TAG", "notification");
+		Log.d("TAG", "notification");
 
-		      /* Invoking the default notification service */
-		      NotificationCompat.Builder  mBuilder = 
-		      new NotificationCompat.Builder(mContext);	
-		      	mBuilder.setContentTitle("Auto Replied");
-		      	mBuilder.setContentText("Text Secretary auto replied to: " + number);
-		      	mBuilder.setTicker("Text Secretary Auto Reply");
-		      	mBuilder.setSmallIcon(R.drawable.ic_action_notification_holo_light);
-		      	mBuilder.setAutoCancel(true);
-		      
-		      /* Creates an explicit intent for an Activity in your app */
-		      Intent resultIntent = new Intent(Intent.ACTION_VIEW, Uri.fromParts("sms", number, null));
+		/* Invoking the default notification service */
+		NotificationCompat.Builder  mBuilder = 
+				new NotificationCompat.Builder(mContext);	
+		mBuilder.setContentTitle("Auto Replied");
+		mBuilder.setContentText("Text Secretary auto replied to: " + number);
+		mBuilder.setTicker("Text Secretary Auto Reply");
+		mBuilder.setSmallIcon(R.drawable.ic_action_notification_holo_light);
+		mBuilder.setAutoCancel(true);
+	      
+		/* Creates an explicit intent for an Activity in your app */
+		Intent resultIntent = new Intent(Intent.ACTION_VIEW, Uri.fromParts("sms", number, null));
+	
+		TaskStackBuilder stackBuilder = TaskStackBuilder.create(mContext);
+		//stackBuilder.addParentStack(NotificationView.class);
+	
+		/* Adds the Intent that starts the Activity to the top of the stack */
+		stackBuilder.addNextIntent(resultIntent);
+		PendingIntent resultPendingIntent =
+				stackBuilder.getPendingIntent(
+						0,
+						PendingIntent.FLAG_UPDATE_CURRENT
+						);
+	         
+		mBuilder.setContentIntent(resultPendingIntent);
 
-		      TaskStackBuilder stackBuilder = TaskStackBuilder.create(mContext);
-		      //stackBuilder.addParentStack(NotificationView.class);
+		mNotificationManager =
+				(NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
 
-		      /* Adds the Intent that starts the Activity to the top of the stack */
-		      stackBuilder.addNextIntent(resultIntent);
-		      PendingIntent resultPendingIntent =
-		         stackBuilder.getPendingIntent(
-		            0,
-		            PendingIntent.FLAG_UPDATE_CURRENT
-		         );
-		         
-		      mBuilder.setContentIntent(resultPendingIntent);
-
-		      mNotificationManager =
-		      (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
-
-		      int notificationID = 100;
-			/* notificationID allows you to update the notification later on. */
-		      mNotificationManager.notify(notificationID , mBuilder.build());
-		   }
+		int notificationID = 100;
+		/* notificationID allows you to update the notification later on. */
+		mNotificationManager.notify(notificationID , mBuilder.build());
+	}
 
 }
