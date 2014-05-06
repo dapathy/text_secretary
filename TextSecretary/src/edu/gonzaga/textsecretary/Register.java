923f7bb5dbd639206463f3fbd42d9ac5d0116421
collnwalkr <ioannes120@gmail.com>
2014-05-06 15:40:53 -0700

    user email now used on database

--- TextSecretary/src/edu/gonzaga/textsecretary/Register.java
+++ TextSecretary/src/edu/gonzaga/textsecretary/Register.java
@@ -8,7 +8,8 @@ import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.app.ProgressDialog;
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
@@ -16,11 +17,12 @@ import android.widget.Toast;
 
 public class Register extends AsyncTask<String, String, String> {
 	private Context mContext;
+	private UserEmailFetcher userEmailFetcher = null;
+	private String userEmail = null;
 	
     public Register (Context context){
          mContext = context;
     }
-	private String user = null;
 	private String paid = null;
 	private String TAG = "Register";
 
@@ -32,7 +34,6 @@ public class Register extends AsyncTask<String, String, String> {
 
     //ids
     private static final String TAG_SUCCESS = "success";
-    private static final String TAG_MESSAGE = "message";
     private static final String TAG_PAID = "paid";
     private static final String TAG_DATE = "trialEND";
 
@@ -42,13 +43,12 @@ public class Register extends AsyncTask<String, String, String> {
 	@Override
 	protected String doInBackground(String... args) {
         int success;
-        user = "9364467121";
         paid = "1";
-        
+        userEmail = UserEmailFetcher.getEmail(mContext);
         try {
             // Building Parameters
             List<NameValuePair> params = new ArrayList<NameValuePair>();
-            params.add(new BasicNameValuePair("userIDD", user));
+            params.add(new BasicNameValuePair("userIDD", userEmail));
             params.add(new BasicNameValuePair("just_paid", paid));
             
             Log.d(TAG, "starting");
@@ -61,21 +61,16 @@ public class Register extends AsyncTask<String, String, String> {
 
             Log.d(TAG, "connected");
 
-            // full json response
             Log.d(TAG, "JSON response" + json.toString());
 
-            // json success element
             success = json.getInt(TAG_SUCCESS);
             if (success == 1) {
-            	//Log.d(TAG, "Success " + json.toString());
             	return (json.getString(TAG_SUCCESS) + " " + json.getString(TAG_PAID) + " " + json.getString(TAG_DATE));
             	}
             else if(success == 2){
-            	//Log.d(TAG, "Success " + json.toString());
             	return (json.getString(TAG_SUCCESS) + " " + json.getString(TAG_PAID) + " " + json.getString(TAG_DATE));
            	}
             else{
-            	//Log.d(TAG, "Failure " +json.getString(TAG_MESSAGE));
             	return (json.getString(TAG_SUCCESS) + " " + json.getString(TAG_PAID) + " " + json.getString(TAG_DATE));
             }
         } catch (JSONException e) {
@@ -91,12 +86,35 @@ public class Register extends AsyncTask<String, String, String> {
 	}
 
     protected void onPostExecute(String file_url) {
-        // dismiss the dialog once product deleted
         if (file_url != null){
         	Log.d(TAG, "onPostExecute: " + file_url);
         	Toast.makeText(mContext, file_url, Toast.LENGTH_LONG).show();
         }
+    }
+    
+    public static class UserEmailFetcher {
+        
+        static String getEmail(Context context) {
+          AccountManager accountManager = AccountManager.get(context); 
+          Account account = getAccount(accountManager);
 
+          if (account == null) {
+            return null;
+          } else {
+            return account.name;
+          }
+        }
+
+        private static Account getAccount(AccountManager accountManager) {
+          Account[] accounts = accountManager.getAccountsByType("com.google");
+          Account account;
+          if (accounts.length > 0) {
+            account = accounts[0];      
+          } else {
+            account = null;
+          }
+          return account;
+        }
     }
 
 
