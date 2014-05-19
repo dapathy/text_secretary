6e11fc6274e5b1a5eb63f19f79fa5c4ca84bdcc9
Tyler <Tyler@tdoster-gx640>
2014-05-18 21:57:31 -0500

    remove unlock check from service, implement check in widget and boot

--- TextSecretary/src/edu/gonzaga/textsecretary/StartOnBoot.java
+++ TextSecretary/src/edu/gonzaga/textsecretary/StartOnBoot.java
@@ -1,20 +1,26 @@
 package edu.gonzaga.textsecretary;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.util.Log;
 
 public class StartOnBoot extends BroadcastReceiver{
-
+	
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		if ("android.intent.action.BOOT_COMPLETED".equals(intent.getAction())) {
 			
 			SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
 			
-	    	if(settings.getBoolean("start_on_boot_preference", false)){
+			//if boot is enabled and product is activated
+	    	if(settings.getBoolean("start_on_boot_preference", false) && isActivated(context)){
 	    		Intent smsService = new Intent(context, SMS_Service.class);
 				context.startService(smsService);
 				
@@ -23,6 +29,33 @@ public class StartOnBoot extends BroadcastReceiver{
 		    	editor.commit();
 	    	}
 		}
+	}
+	
+	//checks unlock status then registers receiver
+	private boolean isActivated(Context context){
+		SharedPreferences secureSettings = new SecurePreferences(context);
+		String account = UserEmailFetcher.getEmail(context);
+		
+		//if application not paid in shared preferences
+		if(secureSettings.getBoolean(account+"_paid", false)){
+			return true;
+		}
+		//not in shared preferences, so check server
+		else{
+	        Register task = new Register(context, false);
+	        task.execute();
+	        try {
+				task.get(1000, TimeUnit.MILLISECONDS);	//wait for async to finish
+				//if paid or in trial, start service
+				if(task.isInTrial())
+					return true;
+			} catch (InterruptedException | ExecutionException
+					| TimeoutException e) {
+				Log.e("SMS", "task.get");
+				e.printStackTrace();
+			}
+		}
+		return false;
 	}
 
 }
