431795f174b6071ea4f70e7ba794a2cf7c4e888f
Tyler Doster <tjdfalcon@gmail.com>
2014-05-27 17:53:35 -0500

    fix purchasing sync with server

--- TextSecretary/src/edu/gonzaga/textsecretary/PrefFrag.java
+++ TextSecretary/src/edu/gonzaga/textsecretary/PrefFrag.java
@@ -1,51 +1,14 @@
 package edu.gonzaga.textsecretary;
 
-import java.util.ArrayList;
-
-import com.android.vending.billing.IInAppBillingService;
-
-import android.app.PendingIntent;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentSender.SendIntentException;
-import android.content.ServiceConnection;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
-import android.os.IBinder;
-import android.os.RemoteException;
 import android.preference.Preference;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceScreen;
-import android.util.Log;
 
 public class PrefFrag extends PreferenceFragment implements OnSharedPreferenceChangeListener{
-	
-	private final String TAG = "PURCHASE";
-	private IInAppBillingService mService;
-	private String PACKAGE_NAME;
-	private Register task;
-
-	ServiceConnection mServiceConn = new ServiceConnection() {
-	   @Override
-	   public void onServiceDisconnected(ComponentName name) {
-	       mService = null;
-	   }
-
-	   @Override
-	   public void onServiceConnected(ComponentName name, 
-	      IBinder service) {
-	       mService = IInAppBillingService.Stub.asInterface(service);
-	       
-			//set visibility of unlock
-	       if(isPurchased()){	//queries purchases
-	    	   PreferenceCategory preferenceCategory = (PreferenceCategory) findPreference("Activation");
-	    	   getPreferenceScreen().removePreference(preferenceCategory);
-	       }
-	   }
-	};
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
@@ -57,11 +20,12 @@ public class PrefFrag extends PreferenceFragment implements OnSharedPreferenceCh
 			Preference messagePreference = findPreference("custom_message_preference");
 	        messagePreference.setEnabled(false);
 		}
-		
-		PACKAGE_NAME = getActivity().getPackageName();
-		getActivity().bindService(new 
-		        Intent("com.android.vending.billing.InAppBillingService.BIND"),
-		                mServiceConn, Context.BIND_AUTO_CREATE);
+
+		//set visibility of unlock
+		if(((SettingsActivity) getActivity()).isPurchased()){	//queries purchases
+    	   PreferenceCategory preferenceCategory = (PreferenceCategory) findPreference("Activation");
+    	   getPreferenceScreen().removePreference(preferenceCategory);
+		}
 	}
 	
 	@Override
@@ -69,23 +33,16 @@ public class PrefFrag extends PreferenceFragment implements OnSharedPreferenceCh
 	    super.onResume();
 	    getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
 	}
+	
 	@Override
 	public void onPause(){
 	    super.onPause();
 	    getPreferenceManager().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
 	}
 	
-	@Override
-	public void onDestroy() {
-	    super.onDestroy();
-	    if (mService != null) {
-	    	getActivity().unbindService(mServiceConn);
-	    }   
-	}
-	
 	public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference key){
 		if(key.toString().equals("Unlock Text Secretary")){
-			purchaseUnlock();
+			((SettingsActivity) getActivity()).purchaseUnlock();
 		}
 		return false;
 	}
@@ -95,66 +52,5 @@ public class PrefFrag extends PreferenceFragment implements OnSharedPreferenceCh
 	        Preference preference = findPreference("custom_message_preference");
 	        preference.setEnabled(!sharedPreferences.getBoolean("calendar_preference", false));
 	    }
-	}
-	
-	//checks if unlock has already been purchased
-	private boolean isPurchased(){
-		SharedPreferences securePreferences = new SecurePreferences(getActivity().getApplicationContext());
-		String account = UserEmailFetcher.getEmail(getActivity().getApplicationContext());
-		//check shared preferences first
-		if(securePreferences.getBoolean(account+"_paid", false))
-			return true;
-		
-		//else query google services
-		else{
-			try {
-				Bundle ownedItems = mService.getPurchases(3, PACKAGE_NAME, "inapp", null);
-
-				int response = ownedItems.getInt("RESPONSE_CODE");
-				if (response == 0) {
-				   ArrayList<String> ownedSkus = ownedItems.getStringArrayList("INAPP_PURCHASE_ITEM_LIST");
-
-				   if(ownedSkus.isEmpty())	//nothing owned
-					   return false;
-				   else
-					   return true;			//something owned
-				}
-			} catch (RemoteException e) {
-				Log.e(TAG, "query failed");
-				e.printStackTrace();
-			}
-			return false;
-		}
-	}
-	
-	//TODO: encrypt payload??
-	private void purchaseUnlock(){
-		try {
-			String payload = UserEmailFetcher.getEmail(getActivity().getApplicationContext());
-			Bundle buyIntentBundle = mService.getBuyIntent(3, PACKAGE_NAME, "text_secretary_unlock", "inapp", payload);
-			PendingIntent pendingIntent = buyIntentBundle.getParcelable("BUY_INTENT");
-			
-			getActivity().startIntentSenderForResult(pendingIntent.getIntentSender(),
-					   1001, new Intent(), Integer.valueOf(0), Integer.valueOf(0),
-					   Integer.valueOf(0));
-			
-			Log.d(TAG, "purchase intent started");
-			storeActivation();
-			Log.d(TAG, "Purchase completed");
-		}catch (SendIntentException e) {
-			Log.e(TAG, "didn't start activity");
-			e.printStackTrace();
-		}
-		 catch (RemoteException e) {
-			Log.e(TAG, "getting intent failed");
-			e.printStackTrace();
-		}
-	}
-	
-	//securely stores data locally, then stores on server
-	private void storeActivation(){
-		//store on server
-		task = new Register(getActivity().getApplicationContext());
-		task.execute(true);
 	}
 }
--- TextSecretary/src/edu/gonzaga/textsecretary/SettingsActivity.java
+++ TextSecretary/src/edu/gonzaga/textsecretary/SettingsActivity.java
@@ -1,22 +1,142 @@
 package edu.gonzaga.textsecretary;
 
-import android.annotation.TargetApi;
-import android.os.Build;
-import android.os.Bundle;
-import android.preference.PreferenceActivity;
+import java.util.ArrayList;
 
-@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+import com.android.vending.billing.IInAppBillingService;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.content.IntentSender.SendIntentException;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.preference.PreferenceActivity;
+import android.util.Log;
+
 public class SettingsActivity extends PreferenceActivity {
+	
+	private final String TAG = "PURCHASE";
+	private IInAppBillingService mService;
+	private String PACKAGE_NAME;
+	private Register task;
+	
+	ServiceConnection mServiceConn = new ServiceConnection() {
+		   @Override
+		   public void onServiceDisconnected(ComponentName name) {
+		       mService = null;
+		   }
+
+		   @Override
+		   public void onServiceConnected(ComponentName name, 
+		      IBinder service) {
+		       mService = IInAppBillingService.Stub.asInterface(service);
+		       //create fragment
+		       getFragmentManager().beginTransaction().replace(android.R.id.content, new PrefFrag()).commit();
+		   }
+		};
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		getFragmentManager().beginTransaction().replace(android.R.id.content, new PrefFrag()).commit();
+		PACKAGE_NAME = getPackageName();
+		
+		bindService(new 
+		        Intent("com.android.vending.billing.InAppBillingService.BIND"),
+		                mServiceConn, Context.BIND_AUTO_CREATE);
 	}
 	
 	@Override
 	protected void onStop(){
     	super.onStop();
+	}
+	
+	@Override
+	public void onDestroy() {
+	    super.onDestroy();
+	    if (mService != null) {
+	    	unbindService(mServiceConn);
+	    }   
+	}
+	
+	//checks if unlock has already been purchased
+	protected boolean isPurchased(){
+		SharedPreferences securePreferences = new SecurePreferences(getApplicationContext());
+		String account = UserEmailFetcher.getEmail(getApplicationContext());
+		//check shared preferences first
+		if(securePreferences.getBoolean(account+"_paid", false))
+			return true;
+		
+		//else query google services
+		else{
+			try {
+				Bundle ownedItems = mService.getPurchases(3, PACKAGE_NAME, "inapp", null);
+				int response = ownedItems.getInt("RESPONSE_CODE");
+				if (response == 0) {
+				   ArrayList<String> ownedSkus = ownedItems.getStringArrayList("INAPP_PURCHASE_ITEM_LIST");
+
+				   if(ownedSkus.isEmpty())	//nothing owned
+					   return false;
+				   else
+					   return true;			//something owned
+				}
+			} catch (RemoteException e) {
+				Log.e(TAG, "query failed");
+				e.printStackTrace();
+			}
+			return false;
+		}
+	}
+	
+	//purchases unlock
+	protected void purchaseUnlock(){
+		try {
+			String payload = UserEmailFetcher.getEmail(getApplicationContext());
+			Bundle buyIntentBundle = mService.getBuyIntent(3, PACKAGE_NAME, "text_secretary_unlock", "inapp", payload);
+			PendingIntent pendingIntent = buyIntentBundle.getParcelable("BUY_INTENT");
+			
+			startIntentSenderForResult(pendingIntent.getIntentSender(),
+					   1001, new Intent(), Integer.valueOf(0), Integer.valueOf(0),
+					   Integer.valueOf(0));
+			
+			Log.d(TAG, "purchase intent started");
+			
+		}catch (SendIntentException e) {
+			Log.e(TAG, "didn't start activity");
+			e.printStackTrace();
+		}
+		 catch (RemoteException e) {
+			Log.e(TAG, "getting intent failed");
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {          	        
+      Log.d(TAG, "onActivityResult");
+		if (resultCode == Activity.RESULT_OK){
+         try {
+            storeActivation();
+            Log.d(TAG, "Purchase completed");
+          }
+          catch (Exception e) {
+             Log.e(TAG, "Failed to parse purchase data.");
+             e.printStackTrace();
+          }
+      }
+      else
+    	  Log.w(TAG, "purchase didn't happen for some reason");
+	}
+	
+	//securely stores data locally, then stores on server
+	private void storeActivation(){
+		//store on server
+		task = new Register(getApplicationContext());
+		task.execute(true);
 	}
 }
