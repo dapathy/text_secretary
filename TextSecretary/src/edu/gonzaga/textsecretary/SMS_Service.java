d9dba45575caa62b6507f63960e0e496c0b23754
Tyler Doster <tjdfalcon@gmail.com>
2014-07-02 23:37:45 -0500

    Now listens for incoming calls and will auto reply if status changes from ringing directly to idle

--- TextSecretary/src/edu/gonzaga/textsecretary/SMS_Service.java
+++ TextSecretary/src/edu/gonzaga/textsecretary/SMS_Service.java
@@ -17,8 +17,10 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.preference.PreferenceManager;
+import android.telephony.PhoneStateListener;
 import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 
 public class SMS_Service extends Service{
@@ -26,6 +28,7 @@ public class SMS_Service extends Service{
 	private final String TAG = "Service";
 	private Calendar_Service calendar;
 	private String defMessage = "Sorry, I'm busy at the moment. I'll get back to you as soon as possible.";
+	private SharedPreferences prefs;
 	private final Notification_Service mnotification = new Notification_Service(SMS_Service.this);
 	private HashMap<String, Long> recentNumbers = new HashMap<String, Long>();
 	
@@ -39,70 +42,113 @@ public class SMS_Service extends Service{
 		super.onCreate();		
 		IntentFilter filter = new IntentFilter();
 		filter.addAction("android.provider.Telephony.SMS_RECEIVED");
+		filter.addAction("android.telephony.TelephonyManager.ACTION_PHONE_STATE_CHANGED");
 		calendar = new Calendar_Service(getApplicationContext());
-		registerReceiver (smsListener, filter);
+		registerReceiver (receiver, filter);
 	}
 	
 	@Override
 	public void onDestroy(){
 		super.onDestroy();
-		unregisterReceiver(smsListener);
+		unregisterReceiver(receiver);
 	}
 	
-	private BroadcastReceiver smsListener = new BroadcastReceiver(){
+	private BroadcastReceiver receiver = new BroadcastReceiver(){
 		@Override
 		public void onReceive(Context context, Intent intent) {
-			SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
-			//check if action is sms and (calendar integration is disabled or is in event)
-			if(intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED") && (!prefs.getBoolean("calendar_preference", true) || calendar.inEvent())){
-				Bundle bundle = intent.getExtras();
-				SmsMessage[] msgs = null;
-				String msg_from = "empty";
-				if (bundle != null){
-					try{
-						Object[] pdus = (Object[]) bundle.get("pdus");
-	                    msgs = new SmsMessage[pdus.length];
-	    				Log.d(TAG, "created msgs");
-						for(int i = 0; i < msgs.length; i++){
-	                        msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
-	                        msg_from = msgs[i].getOriginatingAddress(); 
-	                    }
-						
-						//if timer is disabled or is recent
-        				if (!prefs.getBoolean("sleep_timer_preference", true) || !isRecent(msg_from, Long.valueOf(prefs.getString("list_preference", "1800000")))){
-        			        String message;
-        			        
-        			        //retrieves correct message
-        			        if(prefs.getBoolean("calendar_preference", true)){
-        			        	message = prefs.getString("custom_calendar_message_preference", defMessage);
-        			        	message = getNewMessage(message);	//replaces insertables
-        			        }
-        			        else{
-        			        	message = prefs.getString("custom_message_preference", defMessage);
-        			        }
-        					sendSMS(msg_from, message);
-	        				
-        					//create notification
-        					if(prefs.getBoolean("notification_preference", true))
-        						mnotification.displayNotification(msg_from);
-        					
-		        				final String savefrom = msg_from;
-		        				final String savemessage = message;
-        				    new Handler().postDelayed(new Runnable() {
-	        				        @Override
-	        				        public void run() {
-									storeMessage(savefrom , savemessage);
-	        				        }
-	        				    }, 2000);
-        				}
-						
-					} catch(Exception e){
-						Log.d(TAG, "cought");
+			prefs = PreferenceManager.getDefaultSharedPreferences(context);
+			//calendar integration is disabled or is in event
+			if(!prefs.getBoolean("calendar_preference", true) || calendar.inEvent()){
+				
+				//if call
+				if(intent.getAction().equals("android.telephony.TelephonyManager.ACTION_PHONE_STATE_CHANGED")) {
+					PhoneStateChangeListener pscl = new PhoneStateChangeListener();
+					TelephonyManager tm = (TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE);
+					tm.listen(pscl, PhoneStateListener.LISTEN_CALL_STATE);
+				}
+				
+				//if received SMS
+				if(intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")){
+					Bundle bundle = intent.getExtras();
+					SmsMessage[] msgs = null;
+					String msg_from = "empty";
+					if (bundle != null){
+						try{
+							Object[] pdus = (Object[]) bundle.get("pdus");
+		                    msgs = new SmsMessage[pdus.length];
+		    				Log.d(TAG, "created msgs");
+							for(int i = 0; i < msgs.length; i++){
+		                        msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
+		                        msg_from = msgs[i].getOriginatingAddress(); 
+		                    }
+							
+							handleSMSReply(msg_from);
+							
+						} catch(Exception e){
+							Log.d(TAG, "cought");
+						}
 					}
 				}
 			}
 		}
 	};
+	
+	private class PhoneStateChangeListener extends PhoneStateListener {
+	    private boolean wasRinging = false;
+	    //may need to save number when ringing?
+	    
+	    @Override
+	    public void onCallStateChanged(int state, String incomingNumber) {
+	        switch(state){
+	            case TelephonyManager.CALL_STATE_RINGING:
+	                 Log.d(TAG, "RINGING");
+	                 wasRinging = true;
+	                 break;
+	            case TelephonyManager.CALL_STATE_OFFHOOK:
+	                 Log.d(TAG, "OFFHOOK");
+	                 wasRinging = false;
+	                 break;
+	            case TelephonyManager.CALL_STATE_IDLE:
+	                 Log.d(TAG, "IDLE");
+	                 if (wasRinging){
+	                	 handleSMSReply(incomingNumber);
+	                 }
+	                 wasRinging = false;
+	                 break;
+	        }
+	    }
+	}
+	
+	private void handleSMSReply(String msg_from) {
+		//if timer is disabled or is recent
+		if (!prefs.getBoolean("sleep_timer_preference", true) || !isRecent(msg_from, Long.valueOf(prefs.getString("list_preference", "1800000")))){
+	        String message;
+	        
+	        //retrieves correct message
+	        if(prefs.getBoolean("calendar_preference", true)){
+	        	message = prefs.getString("custom_calendar_message_preference", defMessage);
+	        	message = getNewMessage(message);	//replaces insertables
+	        }
+	        else{
+	        	message = prefs.getString("custom_message_preference", defMessage);
+	        }
+			sendSMS(msg_from, message);
+			
+			//create notification
+			if(prefs.getBoolean("notification_preference", true))
+				mnotification.displayNotification(msg_from);
+			
+			//store message
+			final String savefrom = msg_from;
+			final String savemessage = message;
+		    new Handler().postDelayed(new Runnable() {
+			        @Override
+			        public void run() {
+					storeMessage(savefrom , savemessage);
+			        }
+			    }, 2000);
+		}
+	}
 	
 	//checks if sender has sent a text recently (determined by settings)
 	//stores and updates information in hashmap
