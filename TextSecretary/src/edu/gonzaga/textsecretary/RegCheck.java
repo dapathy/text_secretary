d1ff3a43195bf37c3d0b065a218ee62f8a0326f5
Tyler Doster <tjdfalcon@gmail.com>
2014-09-01 23:40:58 -0700

    Revert "regcheck will now check google purchasing status as a failsafe"
    This reverts commit df6223998a55011136be5f170c42f3cd75a2f768.

--- TextSecretary/src/edu/gonzaga/textsecretary/RegCheck.java
+++ TextSecretary/src/edu/gonzaga/textsecretary/RegCheck.java
@@ -1,43 +1,16 @@
 package edu.gonzaga.textsecretary;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.concurrent.ExecutionException;
 
-import com.android.vending.billing.IInAppBillingService;
-
-import android.content.ComponentName;
 import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
 import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.os.IBinder;
-import android.os.RemoteException;
 import android.util.Log;
 
 public class RegCheck {
 	
-	private static final String TAG = "REGCHECK";
-	
-	private IInAppBillingService mService;
-	private ServiceConnection mServiceConn = new ServiceConnection() {
-		
-	    @Override
-	    public void onServiceDisconnected(ComponentName name) {
-	        mService = null;
-	    }
-
-	    @Override
-	    public void onServiceConnected(ComponentName name, 
-	       IBinder service) {
-	        mService = IInAppBillingService.Stub.asInterface(service);
-	    }
-	};
-	
-	
 	//checks unlock status
-	private boolean isActivatedHelper(Context context){
+	public static boolean isActivated(Context context){
 		SharedPreferences secureSettings = new SecurePreferences(context);
 		String account = UserEmailFetcher.getEmail(context);
 		
@@ -54,46 +27,12 @@ public class RegCheck {
 	        Register task = new Register(context);
 	        task.execute(false);
 	        try {
-				boolean serverActivation = task.get();
-				boolean googlePurchase = isPurchasedGoogle(context);
-				
-				if (!serverActivation && googlePurchase)
-					new Register(context).execute(true);	//purchased in Google, so ensure purchased on server
-				
-				return googlePurchase;
+				return task.get();
 			} catch (InterruptedException | ExecutionException e) {
 				Log.e("SMS", "task.get");
 				e.printStackTrace();
 			}
 		}
 		return false;
-	}
-	
-	private boolean isPurchasedGoogle(Context context) {
-		String packageName = context.getPackageName();
-		
-		context.bindService(new 
-		        Intent("com.android.vending.billing.InAppBillingService.BIND"),
-		                mServiceConn, Context.BIND_AUTO_CREATE);
-		
-		try {
-			Bundle ownedItems = mService.getPurchases(3, packageName, "inapp", null);
-			Log.d(TAG, "getPurchases");
-			int response = ownedItems.getInt("RESPONSE_CODE");
-			if (response == 0) {
-			   ArrayList<String> ownedSkus = ownedItems.getStringArrayList("INAPP_PURCHASE_ITEM_LIST");
-
-			   return !ownedSkus.isEmpty();		//true if not empty (something owned)
-			}
-		} catch (RemoteException e) {
-			Log.e(TAG, "query failed");
-			Log.e(TAG, e.getMessage());
-		}
-		return false;
-	}
-	
-	public static boolean isActivated(Context context) {
-		RegCheck checker = new RegCheck();
-		return checker.isActivatedHelper(context);
 	}
 }
