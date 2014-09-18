189eaf55751fc5d61a523dd46187dc560609f876
Tyler Doster <tjdfalcon@gmail.com>
2014-09-17 22:54:43 -0700

    rewrite to support in app billing 3

--- TextSecretary/src/edu/gonzaga/textsecretary/SettingsActivity.java
+++ TextSecretary/src/edu/gonzaga/textsecretary/SettingsActivity.java
@@ -1,70 +1,52 @@
 package edu.gonzaga.textsecretary;
 
-import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 
-import com.android.vending.billing.IInAppBillingService;
+import edu.gonzaga.textsecretary.inapp.IabHelper;
+import edu.gonzaga.textsecretary.inapp.IabResult;
+import edu.gonzaga.textsecretary.inapp.Inventory;
+import edu.gonzaga.textsecretary.inapp.Purchase;
 
-import android.app.Activity;
-import android.app.PendingIntent;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
 import android.content.SharedPreferences;
-import android.content.IntentSender.SendIntentException;
 import android.os.Bundle;
-import android.os.IBinder;
-import android.os.RemoteException;
 import android.preference.PreferenceActivity;
 import android.util.Log;
 
 public class SettingsActivity extends PreferenceActivity {
 	
 	private static final String TAG = "PURCHASE";
-	private IInAppBillingService mService;
-	private String PACKAGE_NAME;
+	private IabHelper mHelper;
 	private Register task;
-	
-	ServiceConnection mServiceConn = new ServiceConnection() {
-		   @Override
-		   public void onServiceDisconnected(ComponentName name) {
-		       mService = null;
-		   }
-
-		   @Override
-		   public void onServiceConnected(ComponentName name, 
-		      IBinder service) {
-		       mService = IInAppBillingService.Stub.asInterface(service);
-		       //create fragment
-		       getFragmentManager().beginTransaction().replace(android.R.id.content, new PrefFrag()).commit();
-		   }
-		};
+	private static final String UNLOCK_SKU = "text_secretary_unlock";
+	private boolean isUnlocked = false;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-		PACKAGE_NAME = getPackageName();
 		
-		bindService(new 
-		        Intent("com.android.vending.billing.InAppBillingService.BIND"),
-		                mServiceConn, Context.BIND_AUTO_CREATE);
-		Log.d(TAG, "bind");
-	}
-	
-	@Override
-	protected void onStop(){
-    	super.onStop();
+		String base64EncodedPublicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApNk2gDi9RLPvIqu/7bHAHglCv31OY+3JoBYNam6ROslAAT2ZC8TVd3obZKaXkZuU8aa+EP3Br1G210vfQsNvb+nb37z10f5sL3HrKLfxUuqZB9p26El36yAN5AuxAyqNJHH5S5AYcaqelYU3xk6Kj5Z6d701xvoF2V1SbWFGCA9cT5FqWiCcTZ8FSUn/BTHa/zVdJ+coWm6d/VuAzlCmvsMt1oUIzyHk/KBo1x/88BQ7yJw7cYBHd1Ge4EGBI3dTlYR0nM3WJyQj/yTO9pOXmxzp6bPEZCGG5tt/ieFhjlbSN9+nXZSiA9NZuwLmkcVvDflzMzjIRpOgka/9R5xidQIDAQAB";
+		
+		mHelper = new IabHelper(this, base64EncodedPublicKey);
+		
+		mHelper.startSetup(new IabHelper.OnIabSetupFinishedListener() {
+			   public void onIabSetupFinished(IabResult result) {
+			      if (!result.isSuccess()) {
+			         Log.d(TAG, "Problem setting up In-app Billing: " + result);
+			      }
+			      //create fragment if no problems
+			      else
+			    	  getFragmentManager().beginTransaction().replace(android.R.id.content, new PrefFrag()).commit();
+			   }
+			});
 	}
 	
 	@Override
 	public void onDestroy() {
 	    super.onDestroy();
-	    if (mService != null) {
-	    	unbindService(mServiceConn);
-	    	Log.d(TAG, "unbind");
-	    }   
+	    
+	    if (mHelper != null)
+	    	mHelper.dispose();
+	    mHelper = null;
 	}
 	
 	//checks if unlock has already been purchased
@@ -77,64 +59,41 @@ public class SettingsActivity extends PreferenceActivity {
 		
 		//else query google services
 		else{
-			try {
-				Bundle ownedItems = mService.getPurchases(3, PACKAGE_NAME, "inapp", null);
-				Log.d(TAG, "getPurchases");
-				int response = ownedItems.getInt("RESPONSE_CODE");
-				if (response == 0) {
-				   ArrayList<String> ownedSkus = ownedItems.getStringArrayList("INAPP_PURCHASE_ITEM_LIST");
+			mHelper.queryInventoryAsync(
+				new IabHelper.QueryInventoryFinishedListener() {
+				   public void onQueryInventoryFinished(IabResult result, Inventory inventory)   
+				   {
+				      if (result.isFailure()) {
+				         // handle error
+				    	  Log.e(TAG, "error while checking purchase");
+				         return;
+				       }
 
-				   if(ownedSkus.isEmpty())	//nothing owned
-					   return false;
-				   else
-					   return true;			//something owned
-				}
-			} catch (RemoteException e) {
-				Log.e(TAG, "query failed");
-				Log.e(TAG, e.getMessage());
-			}
-			return false;
+				       if(inventory.hasPurchase(UNLOCK_SKU)) {
+				    	   isUnlocked = true;
+				    	   return;
+				       }
+				   }});
+			
+			return isUnlocked;
 		}
 	}
 	
 	//purchases unlock
 	protected void purchaseUnlock(){
-		try {
-			String payload = UserEmailFetcher.getEmail(getApplicationContext());
-			Bundle buyIntentBundle = mService.getBuyIntent(3, PACKAGE_NAME, "text_secretary_unlock", "inapp", payload);
-			PendingIntent pendingIntent = buyIntentBundle.getParcelable("BUY_INTENT");
-			
-			startIntentSenderForResult(pendingIntent.getIntentSender(),
-					   1001, new Intent(), Integer.valueOf(0), Integer.valueOf(0),
-					   Integer.valueOf(0));
-			
-			Log.d(TAG, "purchase intent started");
-			
-		}catch (SendIntentException e) {
-			Log.e(TAG, "didn't start activity");
-			e.printStackTrace();
-		}
-		 catch (RemoteException e) {
-			Log.e(TAG, "getting intent failed");
-			e.printStackTrace();
-		}
-	}
-	
-	@Override
-	public void onActivityResult(int requestCode, int resultCode, Intent data) {          	        
-      Log.d(TAG, "onActivityResult");
-		if (resultCode == Activity.RESULT_OK){
-         try {
-            storeActivation();
-            Log.d(TAG, "Purchase completed");
-          }
-          catch (Exception e) {
-             Log.e(TAG, "Failed to parse purchase data.");
-             e.printStackTrace();
-          }
-      }
-      else
-    	  Log.w(TAG, "purchase didn't happen for some reason");
+		mHelper.launchPurchaseFlow(this, UNLOCK_SKU, 10001,   
+				new IabHelper.OnIabPurchaseFinishedListener() {
+			   public void onIabPurchaseFinished(IabResult result, Purchase purchase) 
+			   {
+			      if (result.isFailure()) {
+			         Log.d(TAG, "Error purchasing: " + result);
+			         return;
+			      }
+			      //purchase successful!
+			      else if (purchase.getSku().equals(UNLOCK_SKU)) {
+			         storeActivation();
+			      }
+			   }}, UserEmailFetcher.getEmail(getApplicationContext()));
 	}
 	
 	//securely stores data locally, then stores on server
