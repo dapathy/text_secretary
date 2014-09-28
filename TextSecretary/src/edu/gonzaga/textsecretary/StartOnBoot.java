9340243f45033f83ec049c0a6f298ca94d257274
Tyler Doster <tjdfalcon@gmail.com>
2014-09-27 19:25:04 -0700

    boot: open app if start on boot enabled, but outside trial

--- TextSecretary/src/edu/gonzaga/textsecretary/StartOnBoot.java
+++ TextSecretary/src/edu/gonzaga/textsecretary/StartOnBoot.java
@@ -5,22 +5,36 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.util.Log;
 
 public class StartOnBoot extends BroadcastReceiver{
+	
+	private static final String TAG = "BOOT";
 	
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		if ("android.intent.action.BOOT_COMPLETED".equals(intent.getAction())) {
-			
+			Log.d(TAG, "boot completed");
 			SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
 			SharedPreferences.Editor editor = settings.edit();
 			
-			//if boot is enabled and product is activated
-	    	if(settings.getBoolean("start_on_boot_preference", false) && RegCheck.isActivated(context)){
-	    		Intent smsService = new Intent(context, SMS_Service.class);
-				context.startService(smsService);
-				
-		    	editor.putBoolean("smsState", true);
+			//if boot is enabled
+	    	if(settings.getBoolean("start_on_boot_preference", false)){
+	    		//if activated, start service
+	    		if (RegCheck.isActivated(context)) {
+	    			Log.d(TAG, "auto start");
+		    		Intent smsService = new Intent(context, SMS_Service.class);
+					context.startService(smsService);				
+			    	editor.putBoolean("smsState", true);
+	    		}
+	    		//otherwise open app to display trial over dialogue
+	    		else {
+	    			Log.d(TAG, "opening main");
+	    			Intent activity = new Intent(context, MainActivity.class);
+	    			activity.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+	    	        context.startActivity(activity);
+	    			editor.putBoolean("smsState", false);
+	    		}
 	    	}
 	    	else{
 	    		editor.putBoolean("smsState", false);
