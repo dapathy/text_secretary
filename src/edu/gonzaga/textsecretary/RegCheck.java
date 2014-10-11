d859cd9d670564f34ee94d8ab094bf1370601335
Collin Walker <ioannes120@gmail.com>
2014-10-11 16:27:43 -0700

    my commit

---
+++ src/edu/gonzaga/textsecretary/RegCheck.java
@@ -0,0 +1,40 @@
+package edu.gonzaga.textsecretary;
+
+import java.util.Date;
+import java.util.concurrent.ExecutionException;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+public class RegCheck {
+	
+	private final static String TAG = "REGCHECK";
+	
+	//checks unlock status
+	public static boolean isActivated(Context context){
+		SharedPreferences secureSettings = new SecurePreferences(context);
+		String account = UserEmailFetcher.getEmail(context);
+		
+		//if application paid in shared preferences
+		if(secureSettings.getBoolean(account+"_paid", false)){
+			return true;
+		}
+		//if current date is less than date stored
+		else if(secureSettings.getLong(account+"_trial", 0) > new Date().getTime()){
+			return true;
+		}
+		//not in shared preferences, so check server
+		else{
+	        Register task = new Register(context);
+	        task.execute(false);
+	        try {
+				return task.get();
+			} catch (InterruptedException | ExecutionException e) {
+				Log.e(TAG, "task.get failed");
+				e.printStackTrace();
+			}
+		}
+		return false;
+	}
+}
