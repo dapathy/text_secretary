81a7003ee508dd107461aba6132e72eec3d3db0f
Tyler Doster <tjdfalcon@gmail.com>
2014-10-26 00:24:49 -0700

    redo activity recognition stuff

---
+++ TextSecretary/src/edu/gonzaga/textsecretary/activity_recognition/ActivityRecognitionIntentService.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package edu.gonzaga.textsecretary.activity_recognition;
+
+import com.google.android.gms.location.ActivityRecognitionResult;
+import com.google.android.gms.location.DetectedActivity;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+/**
+ * Service that receives ActivityRecognition updates. It receives updates
+ * in the background, even if the main Activity is not visible.
+ */
+public class ActivityRecognitionIntentService extends IntentService {
+
+	private int previousActivityType = DetectedActivity.UNKNOWN;
+	
+	public ActivityRecognitionIntentService() {
+        // Set the label for the service's background thread
+        super("ActivityRecognitionIntentService");
+    }
+
+    /**
+     * Called when a new activity detection update is available.
+     */
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+        // If the intent contains an update
+        if (ActivityRecognitionResult.hasResult(intent)) {
+
+            // Get the update
+            ActivityRecognitionResult result = ActivityRecognitionResult.extractResult(intent);
+
+            // Get the most probable activity from the list of activities in the update
+            DetectedActivity mostProbableActivity = result.getMostProbableActivity();
+
+            // Get the confidence percentage for the most probable activity
+            int confidence = mostProbableActivity.getConfidence();
+
+            // Get the type of activity
+            int activityType = mostProbableActivity.getType();
+
+            // If the repository contains a type
+            if (
+                       // If the current type is "moving"
+                       isMoving(activityType)
+
+                       &&
+
+                       // The activity has changed from the previous activity
+                       activityChanged(activityType)
+
+                       // The confidence level for the current activity is > 50%
+                       && (confidence >= 50)) {
+
+                //TODO: broadcast or something.....
+            }
+        }
+    }
+
+    /**
+     * Tests to see if the activity has changed
+     *
+     * @param currentType The current activity type
+     * @return true if the user's current activity is different from the previous most probable
+     * activity; otherwise, false.
+     */
+    private boolean activityChanged(int currentType) {
+        // If the previous type isn't the same as the current type, the activity has changed
+        if (previousActivityType != currentType) {
+            return true;
+
+        // Otherwise, it hasn't.
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Determine if an activity means that the user is moving.
+     *
+     * @param type The type of activity the user is doing (see DetectedActivity constants)
+     * @return true if the user seems to be moving from one location to another, otherwise false
+     */
+    private static boolean isMoving(int type) {
+        switch (type) {
+            // These types mean that the user is probably not moving
+            case DetectedActivity.STILL :
+            case DetectedActivity.TILTING :
+            case DetectedActivity.UNKNOWN :
+                return false;
+            default:
+                return true;
+        }
+    }
+}
---
+++ TextSecretary/src/edu/gonzaga/textsecretary/activity_recognition/ActivityRecognizer.java
@@ -0,0 +1,145 @@
+package edu.gonzaga.textsecretary.activity_recognition;
+
+import android.app.Activity;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+
+import edu.gonzaga.textsecretary.activity_recognition.ActivityUtils.REQUEST_TYPE;
+
+public class ActivityRecognizer extends Service {
+	
+	private REQUEST_TYPE mRequestType;
+	private DetectionRequester mDetectionRequester;
+    private DetectionRemover mDetectionRemover;
+	
+	@Override
+	public void onCreate () {
+        super.onCreate();
+        
+        mDetectionRequester = new DetectionRequester(getApplicationContext());
+        mDetectionRemover = new DetectionRemover(getApplicationContext());
+        
+        startUpdates();
+	}
+    
+    private boolean servicesConnected() {
+        // Check that Google Play services is available
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
+
+        // If Google Play services is available
+        if (ConnectionResult.SUCCESS == resultCode) {
+
+            return true;
+
+        // Google Play services was not available for some reason
+        } else {
+            return false;
+        }
+    }
+	
+	//start listening for updates
+	public void startUpdates() {
+
+        // Check for Google Play services
+        if (!servicesConnected()) {
+            return;
+        }
+
+        /*
+         * Set the request type. If a connection error occurs, and Google Play services can
+         * handle it, then onActivityResult will use the request type to retry the request
+         */
+        mRequestType = ActivityUtils.REQUEST_TYPE.ADD;
+
+        // Pass the update request to the requester object
+        mDetectionRequester.requestUpdates();
+    }
+	
+	//stop checking for activity updates
+	public void stopUpdates() {
+
+        // Check for Google Play services
+        if (!servicesConnected()) {
+            return;
+        }
+
+        /*
+         * Set the request type. If a connection error occurs, and Google Play services can
+         * handle it, then onActivityResult will use the request type to retry the request
+         */
+        mRequestType = ActivityUtils.REQUEST_TYPE.REMOVE;
+
+        // Pass the remove request to the remover object
+        mDetectionRemover.removeUpdates(mDetectionRequester.getRequestPendingIntent());
+
+        /*
+         * Cancel the PendingIntent. Even if the removal request fails, canceling the PendingIntent
+         * will stop the updates.
+         */
+        mDetectionRequester.getRequestPendingIntent().cancel();
+    }
+	
+	/*
+     * Handle results returned to this Activity by other Activities started with
+     * startActivityForResult(). In particular, the method onConnectionFailed() in
+     * DetectionRemover and DetectionRequester may call startResolutionForResult() to
+     * start an Activity that handles Google Play services problems. The result of this
+     * call returns here, to onActivityResult.
+     */
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+
+        // Choose what to do based on the request code
+        switch (requestCode) {
+
+            // If the request code matches the code sent in onConnectionFailed
+            case ActivityUtils.CONNECTION_FAILURE_RESOLUTION_REQUEST :
+
+                switch (resultCode) {
+                    // If Google Play services resolved the problem
+                    case Activity.RESULT_OK:
+
+                        // If the request was to start activity recognition updates
+                        if (ActivityUtils.REQUEST_TYPE.ADD == mRequestType) {
+
+                            // Restart the process of requesting activity recognition updates
+                            mDetectionRequester.requestUpdates();
+
+                        // If the request was to remove activity recognition updates
+                        } else if (ActivityUtils.REQUEST_TYPE.REMOVE == mRequestType ){
+
+                                /*
+                                 * Restart the removal of all activity recognition updates for the 
+                                 * PendingIntent.
+                                 */
+                                mDetectionRemover.removeUpdates(
+                                    mDetectionRequester.getRequestPendingIntent());
+
+                        }
+                    break;
+
+                    // If any other result was returned by Google Play services
+                    default:
+
+                        // Report that Google Play services was unable to resolve the problem.
+                        Log.d(ActivityUtils.APPTAG, "google play couldn't do anything useful");
+                }
+
+            // If any other request code was received
+            default:
+               // Report that this Activity received an unknown requestCode
+               Log.d(ActivityUtils.APPTAG, "I'm not sure what happened but it wasn't good");
+
+               break;
+        }
+    }
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		return null;
+	}
+}
